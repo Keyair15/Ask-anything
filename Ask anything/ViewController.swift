70fa33b8a633e5b36e06a105e332faa8bfc49c4d
KierreTurner <kierreturner16@gmail.com>
2021-07-15 11:19:42 -0500

    Initial Commit

---
+++ Ask anything/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ask anything
+//
+//  Created by Student31 on 7/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Ask anythingTests/Ask_anythingTests.swift
@@ -0,0 +1,33 @@
+//
+//  Ask_anythingTests.swift
+//  Ask anythingTests
+//
+//  Created by Student31 on 7/15/21.
+//
+
+import XCTest
+@testable import Ask_anything
+
+class Ask_anythingTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
