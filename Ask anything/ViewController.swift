455114e7fab1bc85c7799ba5b2228aedaabb8e93
KierreTurner <kierreturner16@gmail.com>
2021-07-18 14:01:26 -0500

    Askmeanything
    assignment about asking an app  yes or no questions

--- Ask anything/ViewController.swift
+++ Ask anything/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var ballArray = [ "ball1", "ball2", "ball3", "ball4", "ball5"]
+      var randomBallNumber = 0
+    
+    @IBOutlet weak var askImageView: UIImageView!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func askButtonPressed(_ sender: Any) {
+        
+        randomBallNumber = Int.random(in: 0...4)
+        
+        askImageView.image = UIImage(named: ballArray[randomBallNumber] )
+         
+        
+    }
+    
 }
 
